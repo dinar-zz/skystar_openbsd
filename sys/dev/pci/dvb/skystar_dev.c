11b599d794358c3709bf71cf3e018f7a5f222a8c
Dinar Talypov <dinar@yantel.ru>
2010-12-02 14:57:54 +0300

    add license to sources

--- sys/dev/pci/dvb/skystar_dev.c
+++ sys/dev/pci/dvb/skystar_dev.c
@@ -1,3 +1,41 @@
+/*
+ * OpenBSD 4.8 driver for Skystar1 DVB card rev. 1.3 and 1.5
+ * based on code by:
+ * Copyright (c) 2002 Alexander Romanov ported to OpenBSD 3.7 for Alloyant Technologies
+ * based on code by:
+ * FreeBSD driver for SkyStar1 DVB card (Siemens Fujitsu DVB PCI)
+ * Copyright (c) 2000 Stanislav "Stephen" Golovin
+ *
+ * Based on linux driver code by:
+ *
+ * Copyright (C) 1999,2000 Ralph  Metzler & Marcus Metzler for convergence integrated media GmbH
+ *
+ * originally based on code by:
+ *
+ * Copyright (C) 1998,1999 Christian Theiss <mistert@rz.fh-augsburg.de>
+ * 
+ * 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Or, point your browser to http://www.gnu.org/copyleft/gpl.html
+ * 
+ * The author can be reached at stas@everest.kaluga.ru
+ *
+ */
+
 #include <sys/param.h>
 #include <sys/ioctl.h>
 #include <sys/mbuf.h>
